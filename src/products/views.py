5c4036ceafa4676625a0fa09d708b06f0af613fa
Justin Mitchel <jmitchel3@gmail.com>
2015-08-18 15:47:07 -0700

    7 - Product Detail View

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,20 @@
+from  django.views.generic.detail import DetailView
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from .models import Product
+
+
+class ProductDetailView(DetailView):
+	model = Product
+
+
+def product_detail_view_func(request, id):
+	product_instance = Product.objects.get(id=id)
+	template = "products/product_detial.html"
+	context = {	
+		"object": product_instance
+	}
+	return render(request, template, context)
