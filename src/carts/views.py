1b1e4b7c293217390bbb052307d80dcd8d29af17
Justin Mitchel <jmitchel3@gmail.com>
2015-08-24 16:49:10 -0700

    39 - Django Sessions

--- src/carts/views.py
+++ src/carts/views.py
@@ -8,14 +8,23 @@ from products.models import Variation
 from carts.models import Cart, CartItem
 
 class CartView(View):
-
 	def get(self, request, *args, **kwargs):
+		request.session.set_expiry(0) #5 minutes
+		cart_id = request.session.get("cart_id")
+		if cart_id == None:
+			cart = Cart()
+			cart.save()
+			cart_id = cart.id
+			request.session["cart_id"] = cart_id
+		cart = Cart.objects.get(id=cart_id)
+		if request.user.is_authenticated():
+			cart.user = request.user
+			cart.save()
 		item_id = request.GET.get("item")
 		delete_item = request.GET.get("delete")
 		if item_id:
 			item_instance = get_object_or_404(Variation, id=item_id)
 			qty = request.GET.get("qty")
-			cart = Cart.objects.all().first()
 			cart_item = CartItem.objects.get_or_create(cart=cart, item=item_instance)[0]
 			if delete_item:
 				cart_item.delete()
