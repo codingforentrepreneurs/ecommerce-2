dcd321713723003ef0dcb2d2aa3d89b8e760e38b
Justin Mitchel <jmitchel3@gmail.com>
2015-08-24 17:29:56 -0700

    41 - Add to Cart from Product View

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,5 +1,5 @@
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import View
 from django.views.generic.detail import SingleObjectMixin
@@ -34,7 +34,12 @@ class CartView(SingleObjectMixin, View):
 		delete_item = request.GET.get("delete")
 		if item_id:
 			item_instance = get_object_or_404(Variation, id=item_id)
-			qty = request.GET.get("qty")
+			qty = request.GET.get("qty", 1)
+			try:
+				if int(qty) < 1:
+					delete_item = True
+			except:
+				raise Http404
 			cart_item = CartItem.objects.get_or_create(cart=cart, item=item_instance)[0]
 			if delete_item:
 				cart_item.delete()
