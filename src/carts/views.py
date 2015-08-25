6947e0e6d30e284c0235cb36b554dd3dad8f4bd4
Justin Mitchel <jmitchel3@gmail.com>
2015-08-24 17:06:36 -0700

    40 - Render Cart View

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,25 +1,35 @@
-from django.views.generic.base import View
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.views.generic.base import View
+from django.views.generic.detail import SingleObjectMixin
+
 
 # Create your views here.
 
 from products.models import Variation
 from carts.models import Cart, CartItem
 
-class CartView(View):
-	def get(self, request, *args, **kwargs):
-		request.session.set_expiry(0) #5 minutes
-		cart_id = request.session.get("cart_id")
+class CartView(SingleObjectMixin, View):
+	model = Cart
+	template_name = "carts/view.html"
+
+	def get_object(self, *args, **kwargs):
+		self.request.session.set_expiry(0) #5 minutes
+		cart_id = self.request.session.get("cart_id")
 		if cart_id == None:
 			cart = Cart()
 			cart.save()
 			cart_id = cart.id
-			request.session["cart_id"] = cart_id
+			self.request.session["cart_id"] = cart_id
 		cart = Cart.objects.get(id=cart_id)
-		if request.user.is_authenticated():
-			cart.user = request.user
+		if self.request.user.is_authenticated():
+			cart.user = self.request.user
 			cart.save()
+		return cart
+
+	def get(self, request, *args, **kwargs):
+		cart = self.get_object()
 		item_id = request.GET.get("item")
 		delete_item = request.GET.get("delete")
 		if item_id:
@@ -31,4 +41,11 @@ class CartView(View):
 			else:
 				cart_item.quantity = qty
 				cart_item.save()
-		return HttpResponseRedirect("/")
+		context = {
+			"object": self.get_object()
+		}
+		template = self.template_name
+		return render(request, template, context)
+
+
+
