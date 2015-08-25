77b1d4ed824640fd18bcb1712f9a7644ac43e3ce
Justin Mitchel <jmitchel3@gmail.com>
2015-08-25 14:00:30 -0700

    46 - Ajax Part 1

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,5 +1,5 @@
 
-from django.http import HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect, Http404, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import View
 from django.views.generic.detail import SingleObjectMixin
@@ -31,7 +31,8 @@ class CartView(SingleObjectMixin, View):
 	def get(self, request, *args, **kwargs):
 		cart = self.get_object()
 		item_id = request.GET.get("item")
-		delete_item = request.GET.get("delete")
+		delete_item = request.GET.get("delete", False)
+		item_added = False
 		if item_id:
 			item_instance = get_object_or_404(Variation, id=item_id)
 			qty = request.GET.get("qty", 1)
@@ -40,12 +41,19 @@ class CartView(SingleObjectMixin, View):
 					delete_item = True
 			except:
 				raise Http404
-			cart_item = CartItem.objects.get_or_create(cart=cart, item=item_instance)[0]
+			cart_item, created = CartItem.objects.get_or_create(cart=cart, item=item_instance)
+			if created:
+				item_added = True
 			if delete_item:
 				cart_item.delete()
 			else:
 				cart_item.quantity = qty
 				cart_item.save()
+		
+		if request.is_ajax():
+			return JsonResponse({"deleted": delete_item, "item_added": item_added}) 
+
+
 		context = {
 			"object": self.get_object()
 		}
