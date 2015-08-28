314cc38464c49dd16783074f25f6ebd5cd58187b
Justin Mitchel <jmitchel3@gmail.com>
2015-08-28 16:06:43 -0700

    59 - Model Choice Field

--- src/orders/forms.py
+++ src/orders/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
+from .models import UserAddress
 User = get_user_model()
 
 class GuestCheckoutForm(forms.Form):
@@ -23,5 +24,14 @@ class GuestCheckoutForm(forms.Form):
 
 
 class AddressForm(forms.Form):
-	billing_address = forms.CharField()
-	shipping_address = forms.CharField()
+	billing_address = forms.ModelChoiceField(
+			queryset=UserAddress.objects.filter(type="billing"),
+			widget = forms.RadioSelect,
+			empty_label = None
+			)
+	shipping_address = forms.ModelChoiceField(
+		queryset=UserAddress.objects.filter(type="shipping"),
+		widget = forms.RadioSelect,
+		empty_label = None,
+		
+		)
--- src/orders/views.py
+++ src/orders/views.py
@@ -3,7 +3,28 @@ from django.views.generic.edit import FormView
 # Create your views here.
 
 from .forms import AddressForm
+from .models import UserAddress
 
 class AddressSelectFormView(FormView):
 	form_class = AddressForm
 	template_name = "orders/address_select.html"
+
+	def get_form(self, *args, **kwargs):
+		form = super(AddressSelectFormView, self).get_form(*args, **kwargs)
+		form.fields["billing_address"].queryset = UserAddress.objects.filter(
+				user__email=self.request.user.email,
+				type='billing',
+			)
+		form.fields["shipping_address"].queryset = UserAddress.objects.filter(
+				user__email=self.request.user.email,
+				type='shipping',
+			)
+		return form
+
+	def form_valid(self, *args, **kwargs):
+		form = super(AddressSelectFormView, self).form_valid(*args, **kwargs)
+		print form
+		return form
+
+	def get_success_url(self, *args, **kwargs):
+		return "/checkout/"
